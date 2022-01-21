46220e8fce87f6a3fbeb1c8d3338bb009145254c
[marmarave] <[marmarave@alum.us.es]>
2022-01-21 12:01:00 +0100

    push de examen

--- src/main/java/org/springframework/samples/petclinic/care/CareController.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareController.java
@@ -1,5 +1,46 @@
 package org.springframework.samples.petclinic.care;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/care")
 public class CareController {
+    @Autowired
+    private CareService careService;
+    private static final String VIWE_CARE_CREAT_OR_UPDATE_FORM = "care/createOrUpdateProvidedCareForm.jsp";
+
+ @GetMapping(path="/create")
+    public String initCreationForm(ModelMap modelMap){
+        String view = VIWE_CARE_CREAT_OR_UPDATE_FORM;
+        modelMap.addAttribute("care", new Care());
+        modelMap.addAttribute("careProvision", careService.getAllCaresProvided());
+        return view;
+
+    }
+
+ @PostMapping(path="/create")
+    public String processCreationForm(@Valid CareProvision care,BindingResult result, ModelMap modelMap) throws NonCompatibleCaresException, UnfeasibleCareException{
+        String view = VIWE_CARE_CREAT_OR_UPDATE_FORM;
+        if(result.hasErrors()){
+        modelMap.addAttribute("careProvision", care);
+        modelMap.addAttribute("careType", careService.getAllCaresProvided());
+        return view;
+    }else{
+        careService.save(care);
+        modelMap.addAttribute("message", "Care sucesfully save");
+       
+    }
+    return "/";
+
+    }
     
 }
+
--- src/main/java/org/springframework/samples/petclinic/care/CareService.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareService.java
@@ -2,25 +2,31 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
+@Service
 public class CareService {    
 
+    @Autowired
+    CareProvisionRepository careProvisionRepository;
+
     public List<Care> getAllCares(){
-        return null;
+        return careProvisionRepository.findAllCares();
     }
 
     public List<Care> getAllCompatibleCares(String petTypeName){
-        return null;
+        return careProvisionRepository.findCompatibleCares(petTypeName);
     }
 
     public Care getCare(String careName) {
-        return null;
+        return careProvisionRepository.findCareByName(careName);
     }
 
     
     public CareProvision save(CareProvision p) throws NonCompatibleCaresException, UnfeasibleCareException {
-        return null;   
+        return careProvisionRepository.save(p);   
     }
 
     public List<CareProvision> getAllCaresProvided(){
